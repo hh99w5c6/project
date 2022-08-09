0b4ebcab90b4d46c4e5997b1a4ed62089f35543c
oneqrhee <blaackpurin@gmail.com>
2022-08-09 12:39:50 +0900

    scheduler 요구사항 변경 적용

--- src/main/java/com/example/hh99_week5/repository/PostRepository.java
+++ src/main/java/com/example/hh99_week5/repository/PostRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByMember(Member member);
+    List<Post> findAllByCommentListIsNull();
 }
--- src/main/java/com/example/hh99_week5/service/Scheduler.java
+++ src/main/java/com/example/hh99_week5/service/Scheduler.java
@@ -2,17 +2,23 @@ package com.example.hh99_week5.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
+import com.example.hh99_week5.entity.Post;
 import com.example.hh99_week5.repository.ImgUrlRepository;
+import com.example.hh99_week5.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class Scheduler {
-
+    private final PostRepository postRepository;
     private final AmazonS3Client amazonS3Client;
     private final ImgUrlRepository imgUrlRepository;
 
@@ -20,7 +26,12 @@ public class Scheduler {
     public String bucket;  // S3 버킷 이름
 
     @Scheduled(cron = "0 0 1 * * *")
-    public void deleteImg(){
+    public void deletePostsNoComment(){
+        List<Post> postsNoComment = postRepository.findAllByCommentListIsNull();
+        for(Post post : postsNoComment){
+            log.info("게시물 <{}>이 삭제되었습니다.", post.getTitle());
+            postRepository.deleteById(post.getId());
+        }
         ObjectListing objects = amazonS3Client.listObjects(bucket, "static");
         do { //1000개 단위로 읽음
             for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
